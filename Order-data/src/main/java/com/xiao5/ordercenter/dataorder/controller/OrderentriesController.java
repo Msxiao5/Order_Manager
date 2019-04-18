60b0fd1fec6c69c340a6bf5ba29fc2527ee1b8a5
tianyda <634258160@qq.com>
2019-04-18 15:19:22 +0800

    订单录入控制

--- Order-data/src/main/java/com/xiao5/ordercenter/dataorder/controller/OrderentriesController.java
+++ Order-data/src/main/java/com/xiao5/ordercenter/dataorder/controller/OrderentriesController.java
@@ -1,9 +1,9 @@
 package com.xiao5.ordercenter.dataorder.controller;
 
+import com.xiao5.ordercenter.dataorder.entity.Orderentries;
 import com.xiao5.ordercenter.dataorder.service.OrderentryService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -20,5 +20,35 @@ import javax.annotation.Resource;
 public class OrderentriesController {
     @Resource
     OrderentryService orderentryService;
+    @GetMapping("/queryOrderByorderentryid/{orderentryid}")
+    public Orderentries queryOrderByorderentryid (@PathVariable("orderentryid") String orderentryid){
+        //调用开始时间
+        long startTimeMillis = System.currentTimeMillis();
+        Orderentries orderentries = orderentryService.queryOrderByorderentryid(orderentryid);
+        if (orderentries == null){
+            log.info("【当前{}orderentryid，无对应订单信息】",orderentryid);
+        }
+        //耗时
+        long timeConsuming = System.currentTimeMillis() - startTimeMillis;
+        log.info("【时长:{}毫秒】", timeConsuming);
+        return orderentries;
+    }
+    @GetMapping("/showOrderList")
+    public Object showOrderListbycutcd(String customercode){
+        return orderentryService.showOrderListbycutcd(customercode);
+    }
+    @GetMapping("/showOrderList")
+    public Object showOrderList(){
+        return orderentryService.showOrderList();
+    }
+    @PostMapping("/Orderentry")
+    public String AddOrderentry(String orderentryid, String customercode, String customername, String custtyp,
+                                String merchandisecode, String merchandisename, String merchandisetype,
+                                String mdseshare, String accno, String currency, String netamount,
+                                String mdsecount, String mediatype, String authmethod, String devicecode,
+                                String channelcode, String channelserialno, String ordersdate, String orderstime,
+                                String crtdatetime, String remark){
+        return orderentryService.AddOrderentry(orderentryid,customercode,customername,custtyp,merchandisecode,merchandisename,merchandisetype,mdseshare,accno,currency,netamount,mdsecount,mediatype,authmethod,devicecode,channelcode,channelserialno,ordersdate,orderstime,crtdatetime,remark);
 
+    }
 }
