fa82f3cc0185a0a6406c9372551d61965d683f64
wutianbing <Ms_xiao5@163.com>
2019-05-30 14:31:08 +0800

    add 注册中心

--- Order-data/src/main/java/com/xiao5/ordercenter/dataorder/DataorderApplication.java
+++ Order-data/src/main/java/com/xiao5/ordercenter/dataorder/DataorderApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -12,6 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @MapperScan("com.xiao5.ordercenter.dataorder.mapper")
 @EntityScan("com.xiao5.ordercenter.common.entity")
 @EnableTransactionManagement
+@EnableEurekaClient
 public class DataorderApplication {
 
     public static void main(String[] args) {
